7ff4bc9370719d497271f3d1c3132261d8ae6519
李朝霞 <lzx@lichaoxiademacbook-pro.local>
2016-07-29 19:33:34 +0800

    Initial Commit

---
+++ 03操作间的依赖/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03操作间的依赖
+//
+//  Created by 李朝霞 on 16/7/29.
+//  Copyright © 2016年 李朝霞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
