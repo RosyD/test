aac63467be133277bed6c09af0240dba9dc13052
李朝霞 <798186625@qq.com>
2016-07-30 20:36:57 +0800

    .......

---
+++ 03GCD的Barrier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03GCD的Barrier
+//
+//  Created by 李朝霞 on 16/7/30.
+//  Copyright © 2016年 李朝霞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 03GCD的Barrier/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  03GCD的Barrier
+//
+//  Created by 李朝霞 on 16/7/30.
+//  Copyright © 2016年 李朝霞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
