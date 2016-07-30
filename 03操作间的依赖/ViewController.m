ea66a1bc699895fac4b1d063ee5fe99ac9b89d75
李朝霞 <798186625@qq.com>
2016-07-30 20:43:53 +0800

    ....

--- 03操作间的依赖/ViewController.m
+++ 03操作间的依赖/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property(nonatomic,strong)NSOperationQueue* queue;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.queue = [[NSOperationQueue alloc]init];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self demo1];
+}
+
+- (void)demo1
+{
+    //登录
+    NSBlockOperation* loginUp = [NSBlockOperation blockOperationWithBlock:^{
+        
+        [NSThread sleepForTimeInterval:3];
+        
+        NSLog(@"登陆成功！ %@",[NSThread currentThread]);
+    }];
+    //下载1
+    
+    NSBlockOperation* down1 = [NSBlockOperation blockOperationWithBlock:^{
+        
+        [NSThread sleepForTimeInterval:2];
+        
+         NSLog(@"down1成功！ %@",[NSThread currentThread]);
+    }];
+    
+    
+    
+      //下载2
+    NSBlockOperation* down2 = [NSBlockOperation blockOperationWithBlock:^{
+        
+        [NSThread sleepForTimeInterval:5];
+        
+        NSLog(@"down2成功！ %@",[NSThread currentThread]);
+    }];
+    
+    
+    [down1 addDependency:loginUp];
+    [down2 addDependency:loginUp];
+    
+    [self.queue addOperations:@[loginUp,down1,down2] waitUntilFinished:false];
+    
 }
 
 - (void)didReceiveMemoryWarning {
